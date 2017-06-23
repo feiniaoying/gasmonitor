a7532ffff61a6c147899e6caa0352b60d3473c8d
chenwt <feiniaoying@163.com>
2017-06-23 13:17:06 +0800

    //swagger接口文档

--- src/main/java/com/zkzj/config/MySwaggerConfig.java
+++ src/main/java/com/zkzj/config/MySwaggerConfig.java
@@ -1,16 +1,17 @@
 package com.zkzj.config;
 
-import com.google.common.base.Predicates;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.ant;
 
 /**
  * Created by chenwt on 2017/6/22.
@@ -19,23 +20,48 @@ import static springfox.documentation.builders.PathSelectors.ant;
 @EnableSwagger2
 public class MySwaggerConfig {
 
-    @Bean
+  /*  @Bean
     public Docket restApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .paths(Predicates.and(ant("/**"), Predicates.not(ant("/error")), Predicates.not(ant("/management/**")), Predicates.not(ant("/management*"))))
+                .paths(Predicates.and(ant("*//**"), Predicates.not(ant("/error")), Predicates.not(ant("/management*//**")), Predicates.not(ant("/management*"))))
                 .build();
     }
-
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("API Test  ")
                 .description("MyAPITest Description")
-                .contact(new Contact("chenwt", "http://www.baidu.com", "feiniaoying@163.com"))
-                .license("Apache 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .contact(new Contact("chenwt", "https://github.com/feiniaoying/gasmonitor", "feiniaoying@163.com"))
+                .license("Gasmonitor 1.0")
+                .licenseUrl("https://github.com/feiniaoying/gasmonitor")
+                .version("1.0.0")
+                .build();
+    }*/
+
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zkzj.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("中科珠江臭气监测API接口Test  ")
+                .description("MyAPITest Description")
+                .contact(new Contact("chenwt", "https://github.com/feiniaoying/gasmonitor", "feiniaoying@163.com"))
+                .license("Gasmonitor 1.0")
+                .licenseUrl("https://github.com/feiniaoying/gasmonitor")
                 .version("1.0.0")
                 .build();
     }
+
+
+
+
 }
